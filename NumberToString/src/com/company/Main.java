a993d96a621a8017359645a2c10954009e0f9b91
Natalia Dorozynska <natala.dorozynska@gmail.com>
2018-08-13 20:40:03 +0200

    number to string

---
+++ NumberToString/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(numberToString(123));
+    }
+    public static String numberToString(int num) {
+        return String.valueOf(num);
+    }
+}
